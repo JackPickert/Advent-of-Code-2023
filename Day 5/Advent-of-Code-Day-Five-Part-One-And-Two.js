ccac01102278349a954904788f291925c0e7d55d
JackPickert <146460993+JackPickert@users.noreply.github.com>
2023-12-18 09:35:56 -0600

    Add files via upload

---
+++ Day 5/Advent-of-Code-Day-Five-Part-One-And-Two.js
@@ -0,0 +1,27 @@
+function waysToWin(...numbers) {
+    const argsArr = [...numbers];
+    let totalWaysToWin = [];
+    for (let i = 0; i < argsArr.length; i += 2) {
+        const numberArr = [];
+        let timeLeftForRace = argsArr[i];
+        let timeHoldingButton = 0;
+        let waysToWin = 0;
+        while (timeLeftForRace > 0) {
+           numberArr.push(timeLeftForRace * timeHoldingButton)
+           timeHoldingButton += 1;
+           timeLeftForRace -= 1;
+        }
+        for (let j = 0; j < numberArr.length; j += 1) {
+            if (numberArr[j] > argsArr[i + 1])
+            waysToWin += 1;
+        }
+        totalWaysToWin.push(waysToWin);
+    }
+
+    let superSum = totalWaysToWin.reduce((accumulator, currentValue) => {
+        return accumulator * currentValue;
+    });
+    console.log(superSum);
+}
+
+waysToWin(35, 212, 93, 2060, 73, 1201, 66, 1044);
