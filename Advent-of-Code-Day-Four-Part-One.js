b311318e26fe917aaea81048f026e82450cb5d5a
JackPickert <146460993+JackPickert@users.noreply.github.com>
2023-12-17 19:44:48 -0600

    Add files via upload

---
+++ Advent-of-Code-Day-Four-Part-One.js
@@ -0,0 +1,33 @@
+function scratchCards(str) {
+    let myBigSum = 0;
+    let mySum = 0;
+    const everyLine = str.split("\n");
+    const cardNumbersRegex = /Card  * *[0-9]*[0-9]*[0-9]:/g; 
+    for (let i = 0; i < everyLine.length; i += 1) {
+        const thisLine = everyLine[i].replace(cardNumbersRegex, "");
+        const leftAndRight = thisLine.split("|")
+        const leftNumbers = leftAndRight[0].match(/[0-9]*[0-9]/g);
+        const rightNumbers = leftAndRight[1].match(/[0-9]*[0-9]/g); 
+        const leftNumbersTrimmedAndParsed = [];
+        const rightNumbersTrimmedAndParsed = []; 
+        for (let j = 0; j < leftNumbers.length; j += 1) {
+            leftNumbersTrimmedAndParsed.push(parseInt(leftNumbers[j]));
+        }
+        for (let k = 0; k < rightNumbers.length; k += 1) {
+            rightNumbersTrimmedAndParsed.push(parseInt(rightNumbers[k]));
+        }
+        let myNum = 0.5;
+        for (let l = 0; l < leftNumbersTrimmedAndParsed.length; l += 1) {
+            for (let m = 0; m < rightNumbersTrimmedAndParsed.length; m += 1) {
+                if (leftNumbersTrimmedAndParsed[l] === rightNumbersTrimmedAndParsed[m]) {
+                    myNum = myNum * 2; 
+                }
+            }
+        }
+        if (myNum >= 1) {
+            myBigSum += myNum; 
+        }
+    }
+    console.log(myBigSum);
+}
+
